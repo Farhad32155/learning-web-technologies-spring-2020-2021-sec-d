c00d87fce03afa2f771c33f71b0b0ece548b88cd
Farhad32155 <farhadhossain3540@gmail.com>
2021-02-18 17:14:52 +0600

    T2A

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_A/E_Check.php
@@ -0,0 +1,21 @@
+<?php
+
+	
+	if(isset($_POST['submit']))
+	{
+
+		$email = $_POST['myemail'];
+		
+
+		if($email == "" ){
+			echo "null submission...";
+		}else{
+			echo $email;
+		}
+
+	}
+	else
+	{
+		echo "invalid request...";
+	}
+?>
